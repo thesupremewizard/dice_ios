70fd0cf16c507bd9a84044e7823338febbf318fa
thesupremewizard <62307175+thesupremewizard@users.noreply.github.com>
2022-08-28 19:40:58 +0300

    Initial Commit

---
+++ dice/diceApp.swift
@@ -0,0 +1,17 @@
+//
+//  diceApp.swift
+//  dice
+//
+//  Created by Roman Smolin on 28.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct diceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
